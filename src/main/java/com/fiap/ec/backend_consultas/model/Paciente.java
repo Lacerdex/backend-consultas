d12bea22c21282f27b3feea85213562b9f62658f
lacerda-note-windows <gabriel.gamexx1@hotmail.com>
2026-03-17 20:57:12 -0300

    Paciente e Especialidade adicionados

--- src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
+++ src/main/java/com/fiap/ec/backend_consultas/model/Paciente.java
@@ -10,20 +10,29 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "pacientes")
 public class Paciente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(nullable = false)
     private String nome;
+
     @Column(nullable = false, unique = true)
     private String cpf;
+
     @Column(nullable = false)
     private String email;
+
     private String telefone;
+
     private LocalDate dataNascimento;
+
     private Boolean ativo;
+
     public Paciente() {
     }
+
     public Paciente(String nome, String cpf, String email,
                     String telefone, LocalDate dataNascimento, Boolean ativo) {
         this.nome = nome;
@@ -33,17 +42,31 @@ public class Paciente {
         this.dataNascimento = dataNascimento;
         this.ativo = ativo;
     }
+
     public Long getId() { return id; }
+
     public String getNome() { return nome; }
+
     public String getCpf() { return cpf; }
+
     public String getEmail() { return email; }
+
     public String getTelefone() { return telefone; }
+
     public LocalDate getDataNascimento() { return dataNascimento; }
+
     public Boolean getAtivo() { return ativo; }
+
     public void setNome(String nome) { this.nome = nome; }
+
     public void setCpf(String cpf) { this.cpf = cpf; }
+
     public void setEmail(String email) { this.email = email; }
+
     public void setTelefone(String telefone) { this.telefone = telefone; }
+
     public void setDataNascimento(LocalDate dataNascimento) { this.dataNascimento = dataNascimento; }
+
     public void setAtivo(Boolean ativo) { this.ativo = ativo; }
+    
 }
